06b1faa3072bc7f83a5eac6944355126c2033902
EsraaMohammadAbdelmoteleb <79087134+EsraaMohammadAbdelmoteleb@users.noreply.github.com>
2023-01-11 10:28:19 +0200

    first update

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+
+// Fixed Nav
+const navBar = document.querySelector(".nav");
+const navHeight = navBar.getBoundingClientRect().height;
+window.addEventListener("scroll", () => {
+  const scrollHeight = window.pageYOffset;
+
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix-nav");
+  } else {
+    navBar.classList.remove("fix-nav");
+  }
+});
+
+var hiddenElement = document.getElementById("about");
+var btn = document.querySelector(".scroll-icon");
+
+function handleButtonClick() {
+    window.scrollTo(0, 400);
+}
+
+btn.addEventListener("click", handleButtonClick);
+// Wrap every letter in a span
+var textWrapper = document.querySelector(".ml3");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".ml3 .letter",
+    opacity: [0, 1],
+    easing: "easeInOutQuad",
+    duration: 1250,
+    delay: (el, i) => 150 * (i + 1),
+  })
+  .add({
+    targets: ".ml3",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000,
+  });

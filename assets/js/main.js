65a1b808119b143e2680f7ebe72b146f4280ba97
EsraaMohammadAbdelmoteleb <79087134+EsraaMohammadAbdelmoteleb@users.noreply.github.com>
2023-01-26 03:37:24 +0200

    updated-secondd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 // Fixed Nav
 const navBar = document.querySelector(".nav");
 const navHeight = navBar.getBoundingClientRect().height;
@@ -11,60 +10,35 @@ window.addEventListener("scroll", () => {
     navBar.classList.remove("fix-nav");
   }
 });
-
+// Scroll Button
 var hiddenElement = document.getElementById("about");
 var btn = document.querySelector(".scroll-icon");
 
 function handleButtonClick() {
-    window.scrollTo(0, 400);
+  window.scrollTo(0, 400);
 }
 
 btn.addEventListener("click", handleButtonClick);
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml3');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml3 .letter',
-    opacity: [0,1],
-    easing: "easeInOutQuad",
-    duration: 2250,
-    delay: (el, i) => 150 * (i+1)
-  }).add({
-    targets: '.ml3',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
-  const loaderContainer = document.querySelector(".loader-container");
+// Spinner
+const loaderContainer = document.querySelector(".loader-container");
 window.addEventListener("load", () => {
-      loaderContainer.classList.add("loader-container-hidden");
+  loaderContainer.classList.add("loader-container-hidden");
 });
-//Get the button
+//Go Up Button
 let mybutton = document.getElementById("btn-back-to-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
-scrollFunction();
+  scrollFunction();
 };
 
 function scrollFunction() {
-if (
-document.body.scrollTop > 20 ||
-document.documentElement.scrollTop > 20
-) {
-mybutton.style.display = "block";
-} else {
-mybutton.style.display = "none";
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
 }
-}
-// When the user clicks on the button, scroll to the top of the document
 mybutton.addEventListener("click", backToTop);
-
 function backToTop() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
-
